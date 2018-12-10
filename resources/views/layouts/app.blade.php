fd8998f61366e579dc2370e9882f33c70130ede8
Exitialis <lpexitialis@gmail.com>
2018-12-10 18:33:56 +0300

    fix icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,8 @@
     {{-- <script src="{{ asset('js/vendor/argon.min.js') }}" defer></script> --}}
 
     <!-- Fonts -->
-    <link href="{{ asset('css/vendor/nucleo.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('css/vendor/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="/css/vendor/nucleo.css" rel="stylesheet" type="text/css">
+    <link href="/css/vendor/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
